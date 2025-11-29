e9c636b82939215b0815a6a2509a1cd976c7b9c7
Rawda Seid <rawdaseali@gmail.com>
2025-11-29 12:07:41 +0300

    Django views and configuration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .models import Library
 from .models import Book
-from django.views.generic import ListView, DetailView
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 
 
 def list_all_books(request):
