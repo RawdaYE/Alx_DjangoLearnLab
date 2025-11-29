75e411ebe0f1eed97238e5946b573dfa3f9e2d8e
Rawda Seid <rawdaseali@gmail.com>
2025-11-29 14:39:21 +0300

    Implementing User Authentication in Django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from .  import views
 from .views import list_books, BookListView, LibraryDetailView
+from .views import RegisterView, LoginView, LogoutView
 
 urlpatterns = [
     path('books/', list_books, name = 'list_all_books'),
     path('books/list/', BookListView.as_view(), name = 'book'),
     path('Library/<int:pk>/', LibraryDetailView.as_view(), name = 'library'),
+
+    path('register/', RegisterView.as_view(), name= 'register'),
+    path('login/', LoginView.as_view(), name= 'login'),
+    path('logout/', LogoutView.as_view(), name= 'logout'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,12 +4,19 @@ from .models import Book
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-def list_all_books(request):
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
+
+
+def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-class List_books(ListView):
+class BookListView(ListView):
     model = Book
     template_name = 'relationship_app/list_books.html'
     context_object_name= 'books'
@@ -19,3 +26,15 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name= 'relationship_app/library_detail.html'
     context_object_name= 'library'
+
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'relationship_app/register.html'
+
+class LoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+
+class LogoutView(LogoutView):
+    next_page = 'login'
