b3c65efa518997eb2a65d424e344c3db9e50fa77
Rawda Seid <rawdaseali@gmail.com>
2025-11-29 09:45:48 +0300

    Implementing model relationships in Django

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -3,10 +3,10 @@ from relationship_app.models import Author, Book, Library, Librarian
 def run_queries():
     author_name = "Author Name"
     try:
-        author = Author.objects.get(name = author_name)
-        books = author.books.all()
+        author = Author.objects.get(name=author_name)
+        books_by_author  = author.books.all()
         print(f"Books written by {author_name}:")
-        for book in books:
+        for book in books_by_author :
             print(f" {book.title}")
     except Author.DoesNotExist:
         print(f"No author found with name '{author_name}'")
@@ -14,7 +14,7 @@ def run_queries():
     
     library_name = "Library Name"
     try:
-        library = Library.objects.get(name = library_name)
+        library = Library.objects.get(name=library_name)
         books_in_library  = library.books.all()
         print(f"Books in {library_name}:")
         for book in books_in_library:
@@ -23,7 +23,7 @@ def run_queries():
         print(f"No library found with name '{library_name}'")
     
     try:
-        library = Library.objects.get(name = library_name)
+        library = Library.objects.get(name=library_name)
         librarian = library.librarian
         print(f"Librarian for {library_name}: {librarian.name}")
     except Library.DoesNotExist:
