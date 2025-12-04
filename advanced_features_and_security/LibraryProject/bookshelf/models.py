4c924caa191b8fa30be835250487c1f546ebf088
Rawda Seid <rawdaseali@gmail.com>
2025-12-04 13:25:37 +0300

    updating the Implementions of a Custom User Model in Django

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,7 +1,6 @@
 from django.db import models
-from .managers import CustomUserManager
-from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
 
 
 # Create your models here.
@@ -13,7 +12,29 @@ class Book(models.Model):
     def __str__(self):
         return self.title
     
-user = get_user_model()
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password= None, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address')
+        email= self.normalize_email(email)
+        user = self.model(username = username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, username, email, password= None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        
+        return self.create_user(username, email, password, **extra_fields)
 
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
