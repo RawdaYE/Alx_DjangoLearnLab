eda08041ff25248626cd1893cc8772733c44c707
Rawda Seid <rawdaseali@gmail.com>
2025-12-17 14:05:07 +0300

    Implementing Security Best Practices in Django

---
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+class BookSearchForm(forms.Form):
+    query = forms.CharField(
+        max_length=100,
+        required=False,
+        widget=forms.TextInput(attrs={'placeholder': 'Search books...'})
+    )
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,14 +1,26 @@
 from django.http import HttpResponse
 from django.contrib.auth.decorators import permission_required
-
+from django.shortcuts import render
+from .models import Book
+from .forms import BookSearchForm
+from django.db.models import Q
 # Create your views here.
 
 
 
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
-        return HttpResponse("Books list page")
+        form = BookSearchForm(request.GET or None)
+        books = Book.objects.all()
+        if form.is_valid():
+           query = form.cleaned_data.get('query', '').strip()
+           if query:
+               # Safe ORM query prevents SQL injection
+               books = books.filter(Q(title__icontains=query) | Q(author__icontains=query))
 
+        return render(request, 'bookshelf/book_list.html', {'books': books, 'form': form})
+        
+        
 @permission_required('bookshelf.can_create', raise_exception=True)
 def add_book(request):
         return HttpResponse("Add book page")
