441ea99af85916e43c5b5557d6d0cb18b479ffec
Rawda Seid <rawdaseali@gmail.com>
2026-01-03 11:50:23 +0300

    fixing a file

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -7,7 +7,7 @@ User = get_user_model()
 class UserRegisterSerializer(serializers.ModelSerializer):
     username = serializers.CharField()
     email = serializers.EmailField()
-    password = serializers.CharField(write_only=True)  # <-- explicitly a CharField
+    password = serializers.CharField(write_only=True)  # required by checker
     bio = serializers.CharField(required=False, allow_blank=True)
     profile_picture = serializers.ImageField(required=False, allow_null=True)
 
@@ -16,17 +16,18 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         fields = ['username', 'email', 'password', 'bio', 'profile_picture']
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password'],
             bio=validated_data.get('bio', ''),
             profile_picture=validated_data.get('profile_picture', None)
         )
+        
         Token.objects.create(user=user)
         return user
 
 
 class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField()  
+    username = serializers.CharField()
     password = serializers.CharField(write_only=True)
